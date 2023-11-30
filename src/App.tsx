8d27e28dc4bb45870b18ec344bfc7402ecf8a1cc
AronNascimento2 <aronviniciusnascimento@gmail.com>
2023-11-30 13:42:30 -0300

    insert alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Header } from "./presentation/components/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    // Executar o alerta somente na primeira renderização
+    if (showAlert) {
+      alert('Este projeto ainda está em andamento, ele pode não funcionar da maneira esperada');
+      setShowAlert(false); // Altera o estado para não exibir o alerta novamente
+    }
+  }, [showAlert]);
   return (
     <>
       <Header />
