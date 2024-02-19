1d275f95dcec9bcc80da40bace96fe02838558e3
aRON <aronviniciusnascimento@gmail.com>
2024-02-19 18:03:51 -0300

    fix: carousl  arrows

--- src/presentation/components/MostSeller/styles.ts
+++ src/presentation/components/MostSeller/styles.ts
@@ -12,7 +12,7 @@ export const Container = styled.div`
   h2 {
     color: #f46f1d;
   }
-  
+
   .prev-button {
     top: 220px;
     right: 20px;
@@ -24,7 +24,29 @@ export const Container = styled.div`
     background-color: #fff;
   }
   .next-button {
-    bottom:355px;
+    bottom:320px;
+    left:97%;
+    position: relative;
+    z-index: 999;
+    width: 40px;
+    height: 60px;
+    border: 1px solid lightgray;
+    background-color: #fff;
+
+  }
+  @media (max-width: 768px) {
+    .prev-button {
+    top: 220px;
+    right: 20px;
+    position: relative;
+    z-index: 999;
+    width: 40px;
+    height: 60px;
+    border: 1px solid lightgray;
+    background-color: #fff;
+  }
+  .next-button {
+    bottom:378px;
     left: 93.8%;
     position: relative;
     z-index: 999;
@@ -33,5 +55,6 @@ export const Container = styled.div`
     border: 1px solid lightgray;
     background-color: #fff;
 
+  }
   }
 `;
